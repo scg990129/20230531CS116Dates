51d3a02923bf5e32f4598eecf4e09459a2575e65
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-05-31 17:07:15 -0700

    Initial Commit

---
+++ 20230531CS116Dates/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  20230531CS116Dates
+//
+//  Created by Jacob Yeung on 5/31/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
